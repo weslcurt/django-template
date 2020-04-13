e6dd83b803ea41f7b58496ee95ddab5324da7221
weslcurt <18757779+weslcurt@users.noreply.github.com>
2020-04-13 17:05:26 -0500

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -23,12 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # The SECRET_KEY is provided via an environment variable in OpenShift
 SECRET_KEY = os.getenv(
     'DJANGO_SECRET_KEY',
-    # safe value used for development when DJANGO_SECRET_KEY might not be set
-    '9e4@&tw46$l31)zrqe3wi+-slqm(ruvz&se0^%9#6(_w3ui!c0'
+    # You must set env var DJANGO_SECRET_KEY = <some random string>
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEBUG_VALUE'] == 'True'
 
 ALLOWED_HOSTS = ['*']
 
